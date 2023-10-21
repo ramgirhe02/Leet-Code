49d2a28b0fc525e8424db688338b15f9f51699d8
ramgirhe02 <138893662+ramgirhe02@users.noreply.github.com>
2023-10-21 17:17:44 +0530

    Time: 0 ms (100%), Space: 40.4 MB (58.5%) - LeetHub

--- 1156-occurrences-after-bigram/1156-occurrences-after-bigram.java
+++ 1156-occurrences-after-bigram/1156-occurrences-after-bigram.java
@@ -1,15 +1,17 @@
 class Solution {
     public String[] findOcurrences(String text, String first, String second) {
         String []  ss =  text.split(" ");
+        ArrayList<String> list = new ArrayList<>();
 
         int count = 0;
         for(int i = 2 ;i<ss.length;i++){
             if(ss[i-2].equals(first) && ss[i-1].equals(second))
-                ss[count++]=ss[i];
+                list.add(ss[i]);
         }
-       String [] s=new String [count];
-       for(int i = 0 ; i<count;i++)
-            s[i]=ss[i];
-        return s;
+    //    String [] s=new String [count];
+    //    for(int i = 0 ; i<count;i++)
+    //         s[i]=ss[i];
+    //l.toArray(new String[0]);
+        return list.toArray(new String[0]);
     }
 }
