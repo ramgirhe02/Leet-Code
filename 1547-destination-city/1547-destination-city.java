3d196333f693faaea9aa0666d8b9ecd655a6a557
ramgirhe02 <138893662+ramgirhe02@users.noreply.github.com>
2023-10-21 12:13:34 +0530

    Time: 5 ms (10.81%), Space: 42.9 MB (45.77%) - LeetHub

--- 1547-destination-city/1547-destination-city.java
+++ 1547-destination-city/1547-destination-city.java
@@ -1,18 +1,33 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        HashMap<String,String> map = new HashMap<>();
-        for(int i = 0 ; i< paths.size();i++){
-            map.put(paths.get(i).get(0),paths.get(i).get(1));
+        Set<String> set = new LinkedHashSet<>();
+        for(List<String> path : paths){
+            set.add(path.get(0));
         }
-        String result = null;
-        for(String s : map.keySet()){
-            while(map.containsKey(map.get(s))){
-                s=map.get(s);
-                result = map.get(s);
+        System.out.println(set);
+        for(List<String> path : paths){
+            if(!set.contains(path.get(1))){
+                return path.get(1);
             }
-            if(result == null)result=map.get(s);
         }
-        //System.out.println(map);
-        return result;
+        return null;
     }
 }
+// class Solution {
+//     public String destCity(List<List<String>> paths) {
+//         HashMap<String,String> map = new HashMap<>();
+//         for(int i = 0 ; i< paths.size();i++){
+//             map.put(paths.get(i).get(0),paths.get(i).get(1));
+//         }
+//         String result = null;
+//         for(String s : map.keySet()){
+//             while(map.containsKey(map.get(s))){
+//                 s=map.get(s);
+//                 result = map.get(s);
+//             }
+//             if(result == null)result=map.get(s);
+//         }
+//         //System.out.println(map);
+//         return result;
+//     }
+// }
