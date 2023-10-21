b5b0e6d301aab4a547228294502b80100a6864f6
ramgirhe02 <138893662+ramgirhe02@users.noreply.github.com>
2023-10-21 11:23:31 +0530

    Time: 71 ms (5.28%), Space: 59.6 MB (37.05%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int arrr[] = arr.clone();
+        Arrays.sort(arrr);
+        int count = 0;
+        Map<Integer,Integer> map = new TreeMap<>();
+        for(int i = 0 ; i<arrr.length;i++){
+            if(!map.containsKey​(arrr[i]))
+                map.put(arrr[i],count++);
+        }
+        for(int j=0;j<arr.length;j++){
+            arr[j]=map.get(arr[j])+1;
+        }
+        return arr;
+        // List<Integer> list = new ArrayList<>();
+        // for(int i = 0 ; i<arr.length;i++){
+        //     if(!list.contains​(arr[i]))
+        //     list.add(arr[i]);
+        // }
+        // Collections.sort(list);
+        // for(int j=0;j<arr.length;j++){
+        //     arr[j]=list.indexOf(arr[j])+1;
+        // }
+        // // System.out.println(list);
+        // return arr;
+    }
+}
