d01263dfcbaadb1f3acdfcebf56ab6f895c5819e
ramgirhe02 <138893662+ramgirhe02@users.noreply.github.com>
2023-10-22 12:33:53 +0530

    Time: 15 ms (100%), Space: 40.9 MB (99.84%) - LeetHub

--- 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.java
+++ 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.java
@@ -1,9 +1,9 @@
 class Solution {
     public int sumOfSquares(int[] nums) {
         int sum = 0;
-        for(int i = 1 ; i<=nums.length;i++){
-            if((nums.length % i) == 0){
-                sum= sum+(nums[i-1]*nums[i-1]);
+        for(int i = 1 ; i <= nums.length ; i++ ){
+            if(nums.length % i == 0){
+                sum= sum+(nums[i-1] * nums[i-1]);
             }
         }
         System.gc();
