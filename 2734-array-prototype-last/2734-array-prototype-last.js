e315c5f29396510443677cbc616c3f45f66bf399
ramgirhe02 <138893662+ramgirhe02@users.noreply.github.com>
2023-10-25 01:37:58 +0530

    Time: 49 ms (61.57%), Space: 41.5 MB (77.67%) - LeetHub

--- 2734-array-prototype-last/2734-array-prototype-last.js
+++ 2734-array-prototype-last/2734-array-prototype-last.js
@@ -4,7 +4,7 @@
 Array.prototype.last = function() {
     if(this.length>0)
         return this[this.length-1];
-    else return -1;
+     return -1;
     //return this.length == 0? -1: this[this.length-1];
 };
 
